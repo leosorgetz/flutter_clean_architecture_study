5d9fdabfaaecee51c28637eb77992b71dc95a5ae
Leonardo Sorgetz <leonardo.monteiro@dragonvc.com.br>
2021-03-22 13:54:35 -0300

    fix and wait for mockito update

--- test/src/domain/data_sources/get_posts/get_posts_data_source_test.dart
+++ test/src/domain/data_sources/get_posts/get_posts_data_source_test.dart
@@ -8,17 +8,17 @@ import 'package:mockito/mockito.dart';
 
 import '../../../utils/mocked_classes.dart';
 
-
 void main() {
   final apiHttpClient = AppHttpClientMock();
   final dataSource = GetPostsDataSource(apiHttpClient);
 
   group('Get Posts Data Source Tests', () {
     test('[Data Source] - Deve retornar uma Lista de Posts para popular a listagem de posts', () async {
-      when(apiHttpClient.get(any)).thenAnswer(
+      when(apiHttpClient.get('')).thenAnswer(
         (_) async => Response(
           data: jsonDecode(json),
           statusCode: HttpStatus.ok,
+          requestOptions: RequestOptions(path: ':path'),
         ),
       );
       final future = dataSource();
